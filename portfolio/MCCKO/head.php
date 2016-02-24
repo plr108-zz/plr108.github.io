9d7b2a9a10adbf318e44bafc56128fe18d6d5516
Patrick Roche <patrick.l.roche@gmail.com>
2016-02-24 13:30:29 -0500

    Add MCC site, Knockout version to portfolio

---
+++ portfolio/MCCKO/head.php
@@ -0,0 +1,28 @@
+<?php
+echo "
+    <meta name=\"description\" content=\"Masterworks Concert Chorale\">
+    <meta charset=\"utf-8\">
+    <title>Masterworks Concert Chorale</title>
+
+    <!--Required for responsiveness-->
+    <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">
+
+    <!--Google Fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
+
+    <!--Font Awesome (for icons)-->
+    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css\">
+    <!--Using Bootstrap-->
+    <link rel=\"stylesheet\" href=\"css/bootstrap.min.css\">
+    <!--Using jQuery, required for Bootstrap-->
+    <script src=\"js/jquery-1.11.3.min.js\"></script>
+
+    <!--Style for snow background-->
+    <link rel=\"stylesheet\" href=\"css/snowstyle.css\">
+
+    <link rel=\"stylesheet\" href=\"css/custom.css\">
+
+    <!--Favicon-->
+    <link rel=\"icon\" href=\"icon16.png\" type=\"image/png\">
+";
+?>
